b5828bd00136473d4d9bddb3ac4a7b243050b25d
Tendai Nyandoro <tnyandoro@gmail.com>
2020-10-15 23:15:25 +0200

    start bubble_sort

---
+++ bubble_sort.rb
@@ -0,0 +1,21 @@
+
+def bubble_sort (arr)
+  n = arr.length
+
+  loop do
+      swapped = false
+   
+      (n-1).times do |i|
+          if arr [i] > arr[i+1]
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+       end
+
+       break if not swapped
+     end
+  end  
+end
+
+
+
+
+bubble_sort([4,3,78,2,0,2])
