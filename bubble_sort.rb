e0ef97c210a9d8c242cafc2c78a82c3a6ad69803
Tendai Nyandoro <tnyandoro@gmail.com>
2020-10-15 23:21:18 +0200

    Add swap method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,13 +3,11 @@ def bubble_sort (arr)
   n = arr.length
 
   loop do
-      swapped = false
-   
-      (n-1).times do |i|
-          if arr [i] > arr[i+1]
+  swapped = false
+   (n-1).times do |i|
+    if arr [i] > arr[i+1]
             arr[i], arr[i + 1] = arr[i + 1], arr[i]
        end
-
        break if not swapped
      end
   end
