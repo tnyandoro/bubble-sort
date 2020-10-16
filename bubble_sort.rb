e1e4c1266707c94ca36cc75f45bbd9479f92ed09
Tendai Nyandoro <tnyandoro@gmail.com>
2020-10-17 00:07:03 +0200

    Resolve error

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,24 +1,29 @@
 def bubble_sort(arr)
+  n = arr.length
+
   loop do
     swapped = false
-    (arr - 1).times do |i|
-      if arr[i] > arr[i + 1]
+
+    (n - 1).times do |i|
+      if arr [i] > arr[i + 1]
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         swapped = true
       end
     end
+
     break unless swapped
   end
+
   arr
 end
 
-p bubble_sort([4, 3, 78, 2, 8, 2, 7, 9, 4, 6, 5])
+p bubble_sort([4, 3, 78, 2, 0, 2])
 
 def bubble_sort_by(arr)
   loop do
     swapped = false
     (arr.length - 1).times do |j|
-      if yield(arr[j], [j + 1]).positive?
+      if yield(arr[j], arr[j + 1]).positive?
         arr[j], arr[j + 1] = arr[j + 1], arr[j]
         swapped = true
       end
@@ -28,4 +33,4 @@ def bubble_sort_by(arr)
   arr
 end
 
-puts bubble_sort_by(%w[hello hi hey how howde well howsit]) { |a, b| a.length - b.length }
+puts bubble_sort_by(%w[hello hi howdie howsit hey]) { |a, b| a.length - b.length }
